552a4eb95463e4103da144b0b9726db6f4d19ccf
hatemAttia <attiahatem@outlook.com>
2021-02-23 22:23:27 +0100

    hii first seance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './shared/component/main-header/main-header.component';
+import { MainFooterComponent } from './shared/component/main-footer/main-footer.component';
+import { ProductsComponent } from './products/products.component';
+import { UsersComponent } from './users/users.component';
+import { routes } from './shared/component/config/router.config';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    MainFooterComponent,
+    ProductsComponent,
+    UsersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
