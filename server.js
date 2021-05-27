b28158d4c58e5781d7180422e38bb2a245613b4f
hatemAttia <attiahatem@outlook.com>
2021-05-28 00:35:47 +0100

    v6

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/angular_tp'));
 
 app.get('/*', function(req, res) {
 
@@ -14,3 +15,4 @@ app.get('/*', function(req, res) {
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+console.log(PORT);
